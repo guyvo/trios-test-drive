9632f829bcc93284b4666c101239f22d539b6b9a
Guy Van Overtveldt <guyvanovertveldt@Mac-Book-Pro-late-2014.local>
2015-01-23 11:53:51 +0100

    initial

---
+++ test/test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Guy Van Overtveldt on 23/01/15.
+//  Copyright (c) 2015 Guy Van Overtveldt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
