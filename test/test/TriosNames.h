c9dd7da9333445b629f82bd8c4a5a798eb529ee4
Guy Van Overtveldt <guyvanovertveldt@Mac-Book-Pro-late-2014.local>
2015-01-23 12:18:23 +0100

    Adding files

---
+++ test/test/TriosNames.h
@@ -0,0 +1,62 @@
+/*
+ *  names.h
+ *  TriosBase
+ *
+ *  Created by Guy Van Overtveldt on 02/10/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ * 
+ */
+
+
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ 
+*/
+
+/**
+ * \defgroup Golbals Global Variables
+ */
+
+/*@{*/
+
+/*! 
+ Light names can be changed here recompile needed\n
+ @todo read in from file
+*/
+const char * gpCLightNames[24] = {
+	"toilet beneden",
+	"berging",
+	"vide draad brug",
+	"garage",
+	"vrij",
+	"living voor",
+	"living midden",
+	"slaapkamer beneden",
+	"slaapkamer 1",
+	"slaapkamer 4",
+	"wand trap",
+	"hal passage",
+	"wand living",
+	"living achter",
+	"keuken tafel",
+	"keuken werkblad",
+	"vrij",
+	"terras buitendeur",
+	"badkamer beneden",
+	"zolder",
+	"badkamer boven",
+	"slaapkamer 2",
+	"slaapkamer 3",
+	"vrij"
+};
+
+/*@}*/
